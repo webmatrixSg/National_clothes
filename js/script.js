1391fe9f77ba86bb97c651d9f912a7b4e7d344e9
SG <sgwebmatriks@gmail.com>
2019-03-15 18:38:38 +0300

    2nd commit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,68 @@
         dots: true,
     });
 
+    function showOrhideModalWindow() {
+        $('#contact-form').fadeToggle();
+        $('#shadow').fadeToggle();
+        $('body').toggleClass('hidden');
+    }
+
+//    Обработчик события клик по кнопке "Заказать"
+    $('.back_call').click(function (event) {
+        event.preventDefault();
+        showOrhideModalWindow();
+
+    });
+
+//    Закрыть форму
+    $('#close-form').click(function (event) {
+        event.preventDefault();
+        showOrhideModalWindow();
+    });
+
+
+
+
+//    Оправка данных из формы обратной связи
+
+    $('form').submit(function (event) {
+        event.preventDefault();
+        var name = $(this).children().eq(0).val();
+        var phone = $(this).children().eq(1).val();
+        $.ajax({
+            url:'mail.php',
+            method:'POST',
+            data:{name:name, phone:phone},
+            success:function (data) {
+                ClearForms();
+                swal("Форма успешно отправлена",'', "success");
+            },
+            error:function(data){
+                console.log(data);
+            }
+        });
+    });
+//  Функция для очистки формы после отправки
+
+    function  ClearForms() {
+        console.log('очистка');
+        for(var i=0; i<$('form').length; i++){
+            $('form').eq(i).children().eq(0).val('');
+            $('form').eq(i).children().eq(1).val('');
+        }
+    }
+
+    //    Скрипт для плавного скролла
+
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+
 
 }());
 
---
+++ mail.php
@@ -0,0 +1,39 @@
+
+<?php
+
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+$name = htmlspecialchars($name);
+$phone = htmlspecialchars($phone);
+
+$name = urldecode($name);
+$phone = urldecode($phone);
+
+$name = trim($name);
+$phone = trim($phone);
+
+$emailAdmin = "sgwebmatriks@gmail.com";
+
+
+$subject = "Заказ с сайта";
+
+$message = ' 
+<html> 
+    <head> 
+        <title>Заявка от клиента</title> 
+        <meta charset="utf-8">
+    </head> 
+    <body> 
+    	<p>Имя: '.$name.'</p>
+    	<p>Номер: '.$phone.'</p>
+
+    </body> 
+</html>';
+
+$headers  = "Content-type: text/html; charset=utf-8 \r\n";
+$headers .= "От кого: Windeco Лэндинг <windeco@webmatrix.by>\r\n";
+
+mail($emailAdmin, $subject, $message, $headers);
+
